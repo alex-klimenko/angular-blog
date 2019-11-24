4638239c4d8baa99068765a605d3e4043f344c21
OM.KLYMENKO <klymenko8olexandr@gmail.com>
2019-11-24 23:11:55 +0200

    add message plugin

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostService } from 'src/app/shared/post.service';
 import { Post } from 'src/app/shared/interfaces';
 import { Subscription } from 'rxjs';
+import { AlertService } from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -12,8 +13,13 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   pSub: Subscription;
+  dSub: Subscription;
+  searchStr = '';
 
-  constructor(private postService: PostService) { }
+  constructor(
+    private postService: PostService,
+    private alert: AlertService
+  ) { }
 
   ngOnInit() {
     this.pSub = this.postService.getAll().subscribe(posts => {
@@ -22,12 +28,19 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   }
 
   remove(id: string) {
-
+    this.dSub = this.postService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id);
+      this.alert.danger('Пост был удален');
+    });
   }
 
   ngOnDestroy() {
     if (this.pSub) {
       this.pSub.unsubscribe();
+    }
+
+    if (this.dSub) {
+      this.dSub.unsubscribe();
     }
   }
 }
--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -3,14 +3,14 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { AuthService } from '../admin/shared/services/auth.service';
 import { Router } from '@angular/router';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(
     private auth: AuthService,
     private router: Router
-    ) {}
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (this.auth.isAuthentificated()) {
@@ -22,9 +22,6 @@ export class AuthInterceptor implements HttpInterceptor {
     }
     return next.handle(req)
       .pipe(
-        tap(() => {
-          console.log('Intercept');
-        }),
         catchError((error: HttpErrorResponse) => {
           console.log('[Interceptor Error]: ', error);
           if (error.status === 401) {
